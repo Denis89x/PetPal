8cd2bde5110675359ffb5b9392bd14f6e4d74dc5
Denis Lebenkov <sabzz1ro3@mail.ru>
2023-09-14 21:01:18 +0300

    New project was created

---
+++ src/main/java/petpal/PetPalApplication.java
@@ -0,0 +1,13 @@
+package petpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petpal/PetPalApplicationTests.java
@@ -0,0 +1,13 @@
+package petpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
